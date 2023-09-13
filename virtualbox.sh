412a1141f4174187777d26c02e8bca0cada9eabb
Geoff Symonds <geoff.sym93@gmail.com>
2023-09-13 13:58:39 -0400

    try again

--- virtualbox.sh
+++ virtualbox.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# install prerequisites
+echo ""
+echo "-----------------------------"
+echo " installing prerequisites... "
+echo "-----------------------------"
+
+sudo apt update
+sudo apt install build-essentials dkms gcc make perl
+
 # run guest additions script
 echo ""
 echo "-----------------------------------"
@@ -7,7 +16,7 @@ echo " running guest additions script... "
 echo "-----------------------------------"
 
 cd /media/$USER/VBox_GAs_*
-. autorun.sh
+sh /media/$USER/VBox_GAs_*/autorun.sh
 
 # set up shared directory
 # https://www.linuxbabe.com/virtualbox/how-to-enable-file-sharing-between-host-and-virtualbox-guest-os
