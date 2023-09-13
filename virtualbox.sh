b545b880a7e99e8053b9853e0e3e167502892314
Geoff Symonds <geoff.sym93@gmail.com>
2023-09-13 13:16:46 -0400

    fix for virtualbox script

--- virtualbox.sh
+++ virtualbox.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# run guest additions script
+echo ""
+echo "-----------------------------------"
+echo " running guest additions script... "
+echo "-----------------------------------"
+
+. /media/$USER/VBox_GAs_*/autorun.sh
+
 # set up shared directory
 # https://www.linuxbabe.com/virtualbox/how-to-enable-file-sharing-between-host-and-virtualbox-guest-os
 echo ""
@@ -9,18 +17,3 @@ echo "--------------------------------"
 
 sudo gpasswd -a $USER vboxsf
 ln -s /media/sf_shared $HOME/shared
-
-# run guest additions script
-echo ""
-echo "-----------------------------------"
-echo " running guest additions script... "
-echo "-----------------------------------"
-
-sh /media/$USER/VBox_GAs_*/autorun.sh
-
-echo ""
-echo "--------------"
-echo " rebooting... "
-echo "--------------"
-
-reboot
